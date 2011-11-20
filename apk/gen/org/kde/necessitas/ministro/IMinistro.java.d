c252418f921630c2c327ba5150ec0d9169fc9e6e
Marco Bernasocchi <marco@placebo.(none)>
2011-11-21 00:45:20 +0100

    updated java part

---
+++ apk/gen/org/kde/necessitas/ministro/IMinistro.java.d
@@ -0,0 +1,4 @@
+/home/marco/dev/qgis-android/apk/gen/org/kde/necessitas/ministro/IMinistro.java: \
+  /home/marco/dev/qgis-android/apk/src/org/kde/necessitas/ministro/IMinistro.aidl \
+  /home/marco/dev/qgis-android/apk/src/org/kde/necessitas/ministro/IMinistroCallback.aidl 
+
---
+++ apk/gen/org/kde/necessitas/ministro/IMinistroCallback.java.d
@@ -0,0 +1,3 @@
+/home/marco/dev/qgis-android/apk/gen/org/kde/necessitas/ministro/IMinistroCallback.java: \
+  /home/marco/dev/qgis-android/apk/src/org/kde/necessitas/ministro/IMinistroCallback.aidl 
+
